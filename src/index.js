32f56df6ec725bb792deb29b286fa88d80ae7820
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-17 09:18:55 -0400

    Whoops

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
   <table>
