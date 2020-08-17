c36fe85b96d72f9e4b86c8bee9a8008473462d9b
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-17 09:28:24 -0400

    Destructure array/object

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
-// CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
+import animals from "./data";
 
-ReactDOM.render(
-  <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top Speed</th>
-    </tr>
-    <tr>
-      <td>{tesla.model}</td>
-      <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td>
-    </tr>
-    <tr>
-      <td>{honda.model}</td>
-      <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
-    </tr>
-  </table>,
-  document.getElementById("root")
-);
+// keywords inside [] can be whatever you want
+const [cat, dog] = animals;
+
+console.log(cat);
+
+// keywords must match the name of properties inside object
+const { name, sound } = cat;
+
+console.log(sound);
+
+// // CHALLENGE: uncomment the code below and see the car stats rendered
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// ReactDOM.render(
+//   <table>
+//     <tr>
+//       <th>Brand</th>
+//       <th>Top Speed</th>
+//     </tr>
+//     <tr>
+//       <td>{tesla.model}</td>
+//       <td>{teslaTopSpeed}</td>
+//       <td>{teslaTopColour}</td>
+//     </tr>
+//     <tr>
+//       <td>{honda.model}</td>
+//       <td>{hondaTopSpeed}</td>
+//       <td>{hondaTopColour}</td>
+//     </tr>
+//   </table>,
+//   document.getElementById("root")
+// );
