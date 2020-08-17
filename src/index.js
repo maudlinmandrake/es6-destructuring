479363f17644045a72fe35933473b8d029d0d9b2
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-17 10:08:13 -0400

    Import objects

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
+import cars from "./practice";
 
 ReactDOM.render(
   <table>
