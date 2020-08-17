0fa4ba3ef3366c0e5773bda848619c23d0a639b7
Jenny Mikac <65610141+maudlinmandrake@users.noreply.github.com>
2020-08-17 10:06:07 -0400

    The actual challenge

--- src/index.js
+++ src/index.js
@@ -1,42 +1,23 @@
+// CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
-import animals from "./data";
 
-// keywords inside [] can be whatever you want
-const [cat, dog] = animals;
-
-console.log(cat);
-
-// keywords must match the name of properties inside object
-// Nested objects
-const {
-  name,
-  sound,
-  feedingRequirements: { food, water }
-} = cat;
-
-console.log(feedingRequirements);
-
-// // CHALLENGE: uncomment the code below and see the car stats rendered
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// ReactDOM.render(
-//   <table>
-//     <tr>
-//       <th>Brand</th>
-//       <th>Top Speed</th>
-//     </tr>
-//     <tr>
-//       <td>{tesla.model}</td>
-//       <td>{teslaTopSpeed}</td>
-//       <td>{teslaTopColour}</td>
-//     </tr>
-//     <tr>
-//       <td>{honda.model}</td>
-//       <td>{hondaTopSpeed}</td>
-//       <td>{hondaTopColour}</td>
-//     </tr>
-//   </table>,
-//   document.getElementById("root")
-// );
+ReactDOM.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top Speed</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+    </tr>
+  </table>,
+  document.getElementById("root")
+);
